a27f01b9b0be52be359d1f15c82c38a3c84967d2
Romeo Namunde <romeomureithi@gmail.com>
2023-12-19 03:46:08 +0300

    fixed circular importation issue

--- deck.h
+++ deck.h
@@ -39,4 +39,6 @@ typedef struct deck_node_s
     struct deck_node_s *next;
 } deck_node_t;
 
+void sort_deck(deck_node_t **deck);
+
 #endif
--- sort.h
+++ sort.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "deck.h"
+#include <string.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -23,7 +23,7 @@ typedef struct listint_s
 
 /* Function prototypes */
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *t);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -36,6 +36,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif
