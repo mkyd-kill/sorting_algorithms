94213c4bd2c6e16e75aab1949ce32f3fe8ef847e
Romeo Namunde <romeomureithi@gmail.com>
2023-12-20 00:23:59 +0300

    sort algorithms

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+/**
+ * cocktail_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Cocktail shaker algorithm
+ * @list: Linked list pointer
+ */
+void cocktail_sort_list(listint_t **list)
+{
+}
---
+++ 102-counting_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * counting_sort - sorts an array of integers in
+ * ascending order using the counting sort algorithm
+ * @array: Array pointer
+ * @size: Size of array
+ */
+void counting_sort(int *array, size_t size)
+{
+}
